7b42befab18172ecbe41d5e7d61afaa8a0d08f28
tylerg05 <tylergardella05@gmail.com>
2020-05-03 15:07:24 -0500

    EF Part 2

--- Program.cs
+++ Program.cs
@@ -12,11 +12,16 @@ namespace BlogsConsole
         {
             var db = new BloggingContext();
             var userInput = "1";
-            while (userInput.Equals("1") || userInput.Equals("2") || userInput.Equals("3"))
+            while (userInput.Equals("1") || userInput.Equals("2") || userInput.Equals("3") || userInput.Equals("4"))
             {
                 Console.WriteLine("1. Display all blogs");
                 Console.WriteLine("2. Add blog");
                 Console.WriteLine("3. Create post");
+                Console.WriteLine("4. Display all posts");
+                Console.WriteLine("5. Edit Blog");
+                Console.WriteLine("6. Edit Post");
+                Console.WriteLine("7. Delete Blog");
+                Console.WriteLine("8. Delete Post");
                 userInput = Console.ReadLine();
                 if (userInput == "1")
                 {
@@ -42,6 +47,7 @@ namespace BlogsConsole
                 }
                 else if (userInput == "3")
                 {
+                    // create a post
                     var query = db.Blogs.OrderBy(b => b.Name);
 
                     var counter = 1;
@@ -62,6 +68,47 @@ namespace BlogsConsole
                     Console.WriteLine("Enter post ID: ");
                     newPost.PostId = Int32.Parse(Console.ReadLine());
                     db.AddPost(selectedBlog, newPost);
+                }
+                else if (userInput == "4")
+                {
+                    // Display all posts from the database
+                    var query = db.Posts.OrderBy(p => p.Title);
+
+                    Console.WriteLine("All posts in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Title);
+                    }
+                }
+                else if (userInput == "5")
+                {
+                    // Edit a blog
+                }
+                else if (userInput == "6")
+                {
+                    // Edit a post
+                }
+                else if (userInput == "7")
+                {
+                    // Delete a blog
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                    var counter = 1;
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(counter + ". " + item.Name);
+                        counter++;
+                    }
+                    Console.WriteLine("Select the blog to delete: ");
+                    var blogSelection = Console.ReadLine();
+                    int x = Int32.Parse(blogSelection);
+                    var selectedBlog = db.Blogs.Find(x);
+                 
+                    
+                }
+                else if (userInput == "8")
+                {
+                    // Delete a post
                 }
                 else
                 {
